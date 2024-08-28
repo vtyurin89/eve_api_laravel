f3a45efecea874767f029f176cab79e47ffecec9
vtyurin89 <vlad89main@gmail.com>
2024-08-28 08:00:54 +0300

    FIXED create entries command

--- app/Console/Commands/CreateEntriesCommands.php
+++ app/Console/Commands/CreateEntriesCommands.php
@@ -24,6 +24,7 @@ class CreateEntriesCommands extends Command
     /**
      * Execute the console command.
      */
+    
     public function handle(CreateEntries $createEntries)
     {
         $createEntries->createAll();
